d353978603f63a97c312fc2c0d7b4f3284392361
Otavio Rodolfo Piske <opiske@redhat.com>
2016-12-20 14:44:33 +0100

    UPD: improves error handling for receipts

--- src/stomp_messenger.c
+++ src/stomp_messenger.c
@@ -1,12 +1,12 @@
 /**
  Copyright 2016 Otavio Rodolfo Piske
- 
+
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
- 
+
  http://www.apache.org/licenses/LICENSE-2.0
- 
+
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -38,7 +38,7 @@ stomp_messenger_t *stomp_messenger_init()
     ret->connection = NULL;
     ret->status.message = NULL;
     stomp_status_success(&ret->status);
-    
+
     status = apr_pool_create(&ret->pool, NULL);
     if (status != APR_SUCCESS) {
         // TODO: collect the error from APR
@@ -57,7 +57,7 @@ stomp_messenger_t *stomp_messenger_init()
         return ret;
     }
 
-    
+
     ret->exchange_properties->hash = (apr_hash_t *) apr_hash_make(ret->pool);
     if (!ret->exchange_properties->hash) {
         stomp_status_set(&ret->status, STOMP_FAILURE,
@@ -65,28 +65,28 @@ stomp_messenger_t *stomp_messenger_init()
 
         return ret;
     }
-    
+
     return ret;
 }
 
 void stomp_messenger_destroy(stomp_messenger_t **messenger)
 {
     free((*messenger)->exchange_properties);
-    
+
     stomp_status_reset(&(*messenger)->status);
-    
-    if ((*messenger) != NULL) { 
+
+    if ((*messenger) != NULL) {
         apr_pool_destroy((*messenger)->pool);
     }
-    
+
     // TODO: disconnect, maybe?
     free(*messenger);
     *messenger = NULL;
-    
+
     apr_terminate();
 }
 
-void stomp_messenger_set_timeout(stomp_messenger_t *messenger, 
+void stomp_messenger_set_timeout(stomp_messenger_t *messenger,
                                               int32_t timeout)
 {
 #if defined(apr_time_from_msec)
@@ -101,14 +101,14 @@ stomp_status_code_t stomp_set_endpoint(stomp_messenger_t *messenger, const char 
     //fprintf(stderr, "Parsing URI: %s\n", uri);
     apr_status_t stat = apr_uri_parse(messenger->pool, uri, &messenger->uri);
     //fprintf(stderr, "Done: %s:%d\n", messenger->uri.hostname, messenger->uri.port);
-    
+
     if (stat != APR_SUCCESS) {
         stomp_status_set(&messenger->status, STOMP_FAILURE,
                 "Unable to parse the URI");
 
         return STOMP_FAILURE;
     }
-    
+
     if (!messenger->uri.path) {
         stomp_status_set(&messenger->status, STOMP_FAILURE,
                 "Invalid connection destination: null");
@@ -122,8 +122,8 @@ stomp_status_code_t stomp_set_endpoint(stomp_messenger_t *messenger, const char 
 
         return STOMP_FAILURE;
     }
-    
-    
+
+
     return STOMP_SUCCESS;
 }
 
@@ -131,22 +131,22 @@ stomp_status_code_t stomp_connect(stomp_messenger_t *messenger,
         stomp_connection_header_t *header, int32_t timeout)
 {
     stomp_frame conn_frame;
-    
+
     fprintf(stderr, "Connecting to %s\n", messenger->uri.hostname);
-    
-    apr_status_t stat = stomp_engine_connect(&messenger->connection, 
+
+    apr_status_t stat = stomp_engine_connect(&messenger->connection,
             messenger->uri.hostname, messenger->uri.port, messenger->pool);
-    
+
     if (stat != APR_SUCCESS) {
         stomp_status_t engine_status = stomp_engine_last_status();
-        
+
         stomp_status_set(&messenger->status, STOMP_FAILURE,
-                "Unable to connect to the broker service: %s", 
+                "Unable to connect to the broker service: %s",
                          engine_status.message);
 
         return STOMP_FAILURE;
     }
-    
+
     stomp_messenger_set_timeout(messenger, timeout);
 
     conn_frame.command = "CONNECT";
@@ -161,7 +161,7 @@ stomp_status_code_t stomp_connect(stomp_messenger_t *messenger,
         }
 
         /*
-         * For now we will just silently ignore the heartbeats until I can 
+         * For now we will just silently ignore the heartbeats until I can
          * investigate further how to implement this properly
          */
     }
@@ -177,14 +177,14 @@ stomp_status_code_t stomp_connect(stomp_messenger_t *messenger,
 
     if (stat != APR_SUCCESS) {
         stomp_status_t engine_status = stomp_engine_last_status();
-        
+
         stomp_status_set(&messenger->status, STOMP_FAILURE,
                 "Unable to write the frame data to the underlying connection: %s",
                          engine_status.message);
 
         return STOMP_FAILURE;
     }
-    
+
     if (!stomp_io_can_read(messenger->connection)) {
         stomp_status_set(&messenger->status, STOMP_FAILURE,
                 "Timed out while trying to read data");
@@ -197,7 +197,7 @@ stomp_status_code_t stomp_connect(stomp_messenger_t *messenger,
     stat = stomp_read(messenger->connection, &reply_frame, messenger->pool);
     if (stat != APR_SUCCESS) {
         stomp_status_t engine_status = stomp_engine_last_status();
-        
+
         stomp_status_set(&messenger->status, STOMP_FAILURE,
                 "Unable to read the frame data to the underlying connection: %s",
                          engine_status.message);
@@ -220,7 +220,7 @@ stomp_status_code_t stomp_connect(stomp_messenger_t *messenger,
 static stomp_status_code_t stomp_process_receipt(stomp_messenger_t *messenger) {
     fprintf(stderr, "Waiting for receipt\n");
     const char *DISCONN_REPLY_STR = "RECEIPT";
-    
+
     if (!stomp_io_can_read(messenger->connection)) {
         stomp_status_set(&messenger->status, STOMP_FAILURE,
                 "Timed out while trying to read data");
@@ -232,7 +232,7 @@ static stomp_status_code_t stomp_process_receipt(stomp_messenger_t *messenger) {
     apr_status_t stat = stomp_read(messenger->connection, &reply_frame, messenger->pool);
     if (stat != APR_SUCCESS) {
         stomp_status_t engine_status = stomp_engine_last_status();
-        
+
         stomp_status_set(&messenger->status, STOMP_FAILURE,
                 "Unable to read the frame data to the underlying connection: %s",
                          engine_status.message);
@@ -242,23 +242,24 @@ static stomp_status_code_t stomp_process_receipt(stomp_messenger_t *messenger) {
 
     if (strncmp(reply_frame->command, DISCONN_REPLY_STR, strlen(DISCONN_REPLY_STR)) != 0) {
         stomp_status_set(&messenger->status, STOMP_FAILURE,
-                "Invalid disconnection reply: %s", reply_frame->command);
+                "Invalid disconnection reply: %s (%s)", reply_frame->command,
+		(reply_frame->body != NULL ? reply_frame->body : "null"));
 
         return STOMP_FAILURE;
     }
-    
+
     return STOMP_SUCCESS;
 }
 
-static inline void stomp_write_receipt(stomp_messenger_t *messenger, 
+static inline void stomp_write_receipt(stomp_messenger_t *messenger,
         stomp_frame *frame, receipt_t receipt) {
-     if (receipt > 0) { 
+     if (receipt > 0) {
         apr_hash_set(frame->headers, "receipt", APR_HASH_KEY_STRING,
                 apr_itoa(messenger->pool, receipt));
     }
 }
 
-stomp_status_code_t stomp_disconnect(stomp_messenger_t *messenger, 
+stomp_status_code_t stomp_disconnect(stomp_messenger_t *messenger,
                                   stomp_disconnection_header_t *header) {
     stomp_frame frame;
 
@@ -268,21 +269,21 @@ stomp_status_code_t stomp_disconnect(stomp_messenger_t *messenger,
     if (header != NULL) {
         stomp_write_receipt(messenger, &frame, header->receipt);
     }
-  
+
     frame.body_length = -1;
     frame.body = NULL;
 
     apr_status_t stat = stomp_write(messenger->connection, &frame, messenger->pool);
     if (stat != APR_SUCCESS) {
         stomp_status_t engine_status = stomp_engine_last_status();
-        
+
         stomp_status_set(&messenger->status, STOMP_FAILURE,
                 "Unable to write the frame data to the underlying connection: %s",
                          engine_status.message);
 
         return STOMP_FAILURE;
     }
-    
+
     if (header != NULL && header->receipt > 0) {
         return stomp_process_receipt(messenger);
     }
@@ -297,10 +298,10 @@ stomp_status_code_t stomp_subscribe(stomp_messenger_t *messenger,
 
     frame.command = "SUBSCRIBE";
     frame.headers = apr_hash_make(messenger->pool);
-    
+
     apr_hash_set(frame.headers, "destination", APR_HASH_KEY_STRING,
             messenger->uri.path);
-    
+
     if (header == NULL) {
         stomp_status_set(&messenger->status, STOMP_FAILURE,
                 "A STOMP subscription requires a subscription ID");
@@ -308,27 +309,27 @@ stomp_status_code_t stomp_subscribe(stomp_messenger_t *messenger,
         return STOMP_FAILURE;
     }
 
-    
+
     apr_hash_set(frame.headers, "id", APR_HASH_KEY_STRING,
             apr_itoa(messenger->pool, header->id));
 
     stomp_write_receipt(messenger, &frame, header->receipt);
-    
-    
+
+
     frame.body_length = -1;
     frame.body = NULL;
-    
+
     apr_status_t stat = stomp_write(messenger->connection, &frame, messenger->pool);
     if (stat != APR_SUCCESS) {
         stomp_status_t engine_status = stomp_engine_last_status();
-        
+
         stomp_status_set(&messenger->status, STOMP_FAILURE,
                 "Unable to write the frame data to the underlying connection: %s",
                          engine_status.message);
 
         return STOMP_FAILURE;
     }
-    
+
     if (header != NULL && header->receipt > 0) {
         return stomp_process_receipt(messenger);
     }
@@ -352,24 +353,24 @@ stomp_status_code_t stomp_unsubscribe(stomp_messenger_t *messenger,
     if (header != NULL) {
         stomp_write_receipt(messenger, &frame, header->receipt);
     }
-    
+
     // TODO: handle the ACK
 
     frame.body_length = -1;
     frame.body = NULL;
-    
-     
+
+
     apr_status_t stat = stomp_write(messenger->connection, &frame, messenger->pool);
     if (stat != APR_SUCCESS) {
         stomp_status_t engine_status = stomp_engine_last_status();
-        
+
         stomp_status_set(&messenger->status, STOMP_FAILURE,
                 "Unable to write the frame data to the underlying connection: %s",
                          engine_status.message);
 
         return STOMP_FAILURE;
     }
-    
+
     if (header != NULL && header->receipt > 0) {
         return stomp_process_receipt(messenger);
     }
@@ -377,18 +378,18 @@ stomp_status_code_t stomp_unsubscribe(stomp_messenger_t *messenger,
     return STOMP_SUCCESS;
 }
 
-static inline void stomp_write_message_id(stomp_messenger_t *messenger, 
-        stomp_frame *frame, 
-        message_id_t message_id) 
+static inline void stomp_write_message_id(stomp_messenger_t *messenger,
+        stomp_frame *frame,
+        message_id_t message_id)
 {
     if (message_id > -1) {
         apr_hash_set(frame->headers, "id", APR_HASH_KEY_STRING,
                 apr_itoa(messenger->pool, message_id));
     }
 }
-static inline void stomp_write_transaction_id(stomp_messenger_t *messenger, 
-        stomp_frame *frame, 
-        transaction_id_t transaction_id) 
+static inline void stomp_write_transaction_id(stomp_messenger_t *messenger,
+        stomp_frame *frame,
+        transaction_id_t transaction_id)
 {
     if (transaction_id > -1) {
         apr_hash_set(frame->headers, "transaction", APR_HASH_KEY_STRING,
@@ -409,7 +410,7 @@ stomp_status_code_t stomp_ack(stomp_messenger_t *messenger,
         stomp_write_transaction_id(messenger, &frame, header->transaction_id);
         stomp_write_receipt(messenger, &frame, header->receipt);
     }
-    
+
     frame.body_length = -1;
     frame.body = NULL;
 
@@ -420,7 +421,7 @@ stomp_status_code_t stomp_ack(stomp_messenger_t *messenger,
 
         return STOMP_FAILURE;
     }
-    
+
     if (header != NULL && header->receipt > 0) {
         return stomp_process_receipt(messenger);
     }
@@ -435,7 +436,7 @@ stomp_status_code_t stomp_nack(stomp_messenger_t *messenger,
 
     frame.command = "NACK";
     frame.headers = apr_hash_make(messenger->pool);
-    
+
     if (header != NULL) {
         stomp_write_message_id(messenger, &frame, header->message_id);
         stomp_write_transaction_id(messenger, &frame, header->transaction_id);
@@ -448,14 +449,14 @@ stomp_status_code_t stomp_nack(stomp_messenger_t *messenger,
     apr_status_t stat = stomp_write(messenger->connection, &frame, messenger->pool);
     if (stat != APR_SUCCESS) {
         stomp_status_t engine_status = stomp_engine_last_status();
-        
+
         stomp_status_set(&messenger->status, STOMP_FAILURE,
                 "Unable to write the frame data to the underlying connection: %s",
                          engine_status.message);
 
         return STOMP_FAILURE;
     }
-    
+
     if (header != NULL && header->receipt > 0) {
         return stomp_process_receipt(messenger);
     }
@@ -476,21 +477,21 @@ static stomp_status_code_t stomp_transaction(stomp_messenger_t *messenger,
         stomp_write_transaction_id(messenger, &frame, header->transaction_id);
         stomp_write_receipt(messenger, &frame, header->receipt);
     }
-    
+
     frame.body_length = -1;
     frame.body = NULL;
 
     apr_status_t stat = stomp_write(messenger->connection, &frame, messenger->pool);
     if (stat != APR_SUCCESS) {
         stomp_status_t engine_status = stomp_engine_last_status();
-        
+
         stomp_status_set(&messenger->status, STOMP_FAILURE,
                 "Unable to write the frame data to the underlying connection: %s",
                          engine_status.message);
 
         return STOMP_FAILURE;
     }
-    
+
     if (header != NULL && header->receipt > 0) {
         return stomp_process_receipt(messenger);
     }
@@ -516,39 +517,39 @@ stomp_status_code_t stomp_abort(stomp_messenger_t *messenger,
     return stomp_transaction(messenger, header, "ABORT");
 }
 
-stomp_status_code_t stomp_send(stomp_messenger_t *messenger, 
-                                  stomp_send_header_t *header, 
+stomp_status_code_t stomp_send(stomp_messenger_t *messenger,
+                                  stomp_send_header_t *header,
                                   stomp_message_t *message)
 {
     stomp_frame frame;
     frame.command = "SEND";
     frame.headers = apr_hash_copy(messenger->pool, messenger->exchange_properties->hash);
-    
-    
-    apr_hash_set(frame.headers, "destination", APR_HASH_KEY_STRING, 
+
+
+    apr_hash_set(frame.headers, "destination", APR_HASH_KEY_STRING,
             messenger->uri.path);
-    
+
 
     frame.body_length = message->size;
     frame.body = message->body;
-    
+
     if (header != NULL) {
         stomp_write_transaction_id(messenger, &frame, header->transaction_id);
         stomp_write_receipt(messenger, &frame, header->receipt);
     }
-    
-   
+
+
     apr_status_t stat = stomp_write(messenger->connection, &frame, messenger->pool);
     if (stat != APR_SUCCESS) {
         stomp_status_t engine_status = stomp_engine_last_status();
-        
+
         stomp_status_set(&messenger->status, STOMP_FAILURE,
                 "Unable to write the frame data to the underlying connection: %s",
                          engine_status.message);
 
         return STOMP_FAILURE;
     }
-    
+
     if (header != NULL && header->receipt > 0) {
         return stomp_process_receipt(messenger);
     }
@@ -557,40 +558,40 @@ stomp_status_code_t stomp_send(stomp_messenger_t *messenger,
     return STOMP_SUCCESS;
 }
 
-stomp_status_code_t stomp_receive(stomp_messenger_t *messenger, 
+stomp_status_code_t stomp_receive(stomp_messenger_t *messenger,
                                   stomp_receive_header_t *header,
                                   stomp_message_t *message)
 {
     stomp_frame *frame;
-       
+
     if (!stomp_io_can_read(messenger->connection)) {
         stomp_status_set(&messenger->status, STOMP_FAILURE,
                 "Timed out while trying to read data");
 
         return STOMP_FAILURE;
     }
-    
+
     apr_status_t stat = stomp_read(messenger->connection, &frame, messenger->pool);
     if (stat == APR_SUCCESS) {
         stomp_message_format(message, frame->body, frame->body_length);
-        messenger->exchange_properties->hash = apr_hash_overlay(messenger->pool, 
+        messenger->exchange_properties->hash = apr_hash_overlay(messenger->pool,
                 frame->headers, messenger->exchange_properties->hash);
-        
+
         if (strncmp(frame->command, "MESSAGE", strlen("MESSAGE")) == 0) {
             return STOMP_SUCCESS;
         }
-        
+
         return STOMP_FAILURE;
     }
     else {
         if (stat == APR_TIMEUP) {
             return STOMP_SUCCESS;
         }
-        
+
         stomp_status_set(&messenger->status, STOMP_FAILURE,
                 "Unable to read the frame data to the underlying connection");
 
         return STOMP_FAILURE;
     }
-    
+
 }
