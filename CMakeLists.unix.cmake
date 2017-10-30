e2eda77705e325a7f8406a00cbfdd198a731bfac
Otavio Rodolfo Piske <opiske@redhat.com>
2017-10-30 12:37:38 +0100

    REF: reorganizing the cmake files

--- CMakeLists.unix.cmake
+++ CMakeLists.unix.cmake
@@ -6,3 +6,26 @@ find_library(GRU_LIB NAMES gru-0
 
 message(STATUS "GRU headers found at ${GRU_INCLUDE_DIR}")
 message(STATUS "GRU library found at ${GRU_LIB}")
+
+
+find_path(APR_INCLUDE_DIR apr_general.h
+        PATH_SUFFIXES apr-1 apr-1.0)
+find_library(APR_LIB NAMES apr-1)
+
+message(STATUS "APR headers found at ${APR_INCLUDE_DIR}")
+message(STATUS "APR library found at ${APR_LIB}")
+
+find_path(APR_UTIL_INCLUDE_DIR apu.h
+        PATH_SUFFIXES apr-1 apr-1.0)
+find_library(APR_UTIL_LIB NAMES aprutil-1)
+
+message(STATUS "APR util headers found at ${APR_UTIL_INCLUDE_DIR}")
+message(STATUS "APR util library found at ${APR_UTIL_LIB}")
+
+include_directories(
+        ${APR_INCLUDE_DIR}
+        ${APR_UTIL_INCLUDE_DIR}
+        ${GRU_INCLUDE_DIR}
+        src
+        src/common
+        src/transport)
