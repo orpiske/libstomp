91d13feaf91cc8ff6325e5876c9e69a5d8b121e1
Otavio Rodolfo Piske <opiske@redhat.com>
2017-10-31 13:44:54 +0100

    FIX: incorrect date on license header

--- src/common/ls_frame.h
+++ src/common/ls_frame.h
@@ -1,5 +1,5 @@
 /**
- Copyright 2016 Otavio Rodolfo Piske
+ Copyright 2017 Otavio Rodolfo Piske
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
